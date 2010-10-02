b5d86ba4ef5720b55edb5430d0a6b2073b636f95
Jeff Mitchell <mitchell@kde.org>
2010-10-02 19:55:02 -0400

    Sync more metadata

--- bin/push_projects_metadata.sh
+++ bin/push_projects_metadata.sh
@@ -9,5 +9,8 @@ for dir in `cat projects-repos.tmp`
     echo "$dir/git-daemon-export-ok" >> projects-repos-build.tmp
     echo "$dir/description" >> projects-repos-build.tmp
     echo "$dir/kde-cloned-from" >> projects-repos-build.tmp
+    echo "$dir/gl-creater" >> projects-repos-build.tmp
+    echo "$dir/gl-owner" >> projects-repos-build.tmp
+    echo "$dir/gl-perms" >> projects-repos-build.tmp
 done
 rsync -avz --password-file=/home/git/rsync_push_to_projects.secret --files-from="/home/git/projects-repos-build.tmp" /srv/kdegit/repositories git-projects@localhost::metadata-tree
