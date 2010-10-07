09c0f627a05f9dbf4e04ee02339d6fab40dd785a
Jeff Mitchell <mitchell@kde.org>
2010-10-07 15:53:46 -0400

    Update commit handler to find whether a url is redmine or not even with a repoid

--- bin/handle_commit_mappings.rb
+++ bin/handle_commit_mappings.rb
@@ -33,13 +33,25 @@ helpers do
     return nil
   end
 
-  def findGitwebUrl(repoid, changeset)
+  def findGitwebOrRedmineUrl(repoid, changeset)
     if not File.exists?("/home/git/repo-uid-mappings/#{repoid}")
-      return nil
+      return findRedmineUrl(changeset)
     end
     path = File.read("/home/git/repo-uid-mappings/#{repoid}").chomp
+    puts path
     if not File.exists?("/repositories/#{path}")
       return nil
+    end
+    execstring = "select identifier, url from projects LEFT JOIN repositories on projects.id = repositories.project_id LEFT JOIN changesets on changesets.repository_id = repositories.id where changesets.revision = '#{changeset}';"
+    res = $pg.exec(execstring)
+    puts res
+    if not res[0].nil?
+      reppath = "/repositories/" + path
+      puts reppath
+      puts res[0][1]
+      if reppath == res[0][1]
+        return "http://projects.kde.org/projects/#{res[0][0]}/repository/revisions/#{changeset}"
+      end
     end
     return "http://gitweb.kde.org/#{path}/commit/#{changeset}"
   end
@@ -47,7 +59,7 @@ helpers do
 end
 
 get %r{/([a-zA-Z0-9]+)/([a-zA-Z0-9]+)} do |repoid, changeset|
-  url = findGitwebUrl(repoid, changeset)
+  url = findGitwebOrRedmineUrl(repoid, changeset)
   if url.nil?
     redirect "http://projects.kde.org/"
   else
