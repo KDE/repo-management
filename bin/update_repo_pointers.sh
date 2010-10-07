a6195cd71151a99e50d59f98f06228159d8c3660
Jeff Mitchell <mitchell@kde.org>
2010-10-07 00:08:11 -0400

    Add update_repo_pointers

---
+++ bin/update_repo_pointers.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /home/git/repo-uid-mappings
+for repouid in $( find /home/git/metadata-tree -name "kde-repo-uid" ); do
+    uid=$(cat $repouid)
+    dir=$(dirname $repouid)
+    dir=${dir#/home/git/metadata-tree}
+    ln -sf /repositories$dir $uid
+done
