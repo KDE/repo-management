6a15c1cf071ca9589ddb6ef73418cdfb273e9464
Jeff Mitchell <mitchell@kde.org>
2010-10-06 23:52:00 -0400

    Remove now-unnecessary debug

--- bin/handle_commit_mappings.rb
+++ bin/handle_commit_mappings.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 require 'postgres'
 require 'sinatra'
-require 'pp'
 
 postgresuser = "commitsscript"
 postgrespass = File.read("/home/git/commit_script_pgpass").chomp
@@ -10,10 +9,7 @@ $pg = PGconn.connect("127.0.0.1", 5432, '', '', "redmine", postgresuser, postgre
 helpers do
   def findRedmineUrl(changeset)
     execstring = "select identifier from projects LEFT JOIN repositories on projects.id = repositories.project_id LEFT JOIN changesets on changesets.repository_id = repositories.id where changesets.revision = '#{changeset}';"
-    pp execstring
     res = $pg.exec(execstring)
-    pp res
-    pp res[0].nil?
     if not res[0].nil?
       return "http://projects.kde.org/projects/#{res[0][0]}/repository/revisions/#{changeset}"
     end
