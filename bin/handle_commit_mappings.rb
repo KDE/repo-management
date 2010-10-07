07fa3dc78017bed88c170a6b5db125191251e3fd
Jeff Mitchell <mitchell@kde.org>
2010-10-07 15:55:13 -0400

    Remove non-repo-uid-containing url support; it'll become broken at some point, so don't let people get used to it...

--- bin/handle_commit_mappings.rb
+++ bin/handle_commit_mappings.rb
@@ -24,18 +24,10 @@ postgrespass = File.read("/home/git/commit_script_pgpass").chomp
 $pg = PGconn.connect("127.0.0.1", 5432, '', '', "redmine", postgresuser, postgrespass)
 
 helpers do
-  def findRedmineUrl(changeset)
-    execstring = "select identifier from projects LEFT JOIN repositories on projects.id = repositories.project_id LEFT JOIN changesets on changesets.repository_id = repositories.id where changesets.revision = '#{changeset}';"
-    res = $pg.exec(execstring)
-    if not res[0].nil?
-      return "http://projects.kde.org/projects/#{res[0][0]}/repository/revisions/#{changeset}"
-    end
-    return nil
-  end
 
   def findGitwebOrRedmineUrl(repoid, changeset)
     if not File.exists?("/home/git/repo-uid-mappings/#{repoid}")
-      return findRedmineUrl(changeset)
+      return nil
     end
     path = File.read("/home/git/repo-uid-mappings/#{repoid}").chomp
     puts path
@@ -60,15 +52,6 @@ end
 
 get %r{/([a-zA-Z0-9]+)/([a-zA-Z0-9]+)} do |repoid, changeset|
   url = findGitwebOrRedmineUrl(repoid, changeset)
-  if url.nil?
-    redirect "http://projects.kde.org/"
-  else
-    redirect url
-  end
-end
-
-get %r{/([a-zA-Z0-9]+)} do |changeset|
-  url = findRedmineUrl(changeset)
   if url.nil?
     redirect "http://projects.kde.org/"
   else
