d45d5e329cd6a757928f858fa6380eb4edc2ee9b
Jeff Mitchell <mitchell@kde.org>
2010-10-07 00:19:50 -0400

    Remove one more / at the beginning

--- bin/update_repo_pointers.sh
+++ bin/update_repo_pointers.sh
@@ -4,6 +4,6 @@ cd /home/git/repo-uid-mappings
 for repouid in $( find /home/git/metadata-tree -name "kde-repo-uid" ); do
     uid=$(cat $repouid)
     dir=$(dirname $repouid)
-    dir=${dir#/home/git/metadata-tree}
+    dir=${dir#/home/git/metadata-tree/}
     echo $dir > $uid
 done
