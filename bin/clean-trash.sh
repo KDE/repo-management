17989a64a26097d97b1ebb9d51394f456918fc4e
Jeff Mitchell <mitchell@kde.org>
2010-10-27 16:41:59 -0400

    Fix typo in clean-trash command

--- bin/clean-trash.sh
+++ bin/clean-trash.sh
@@ -5,5 +5,5 @@ cutoff=`date -I -d '28 days ago'`
 find $TRASH_CAN -type d -name "20??-??-??_*" | while read r
 do
     d=`basename $r`
-    [[ $d < $cutoff ]] && rm -rf $d
+    [[ $d < $cutoff ]] && rm -rf $r
 done
