09a5d998824f51de8a1e6c31d1d6e64db96d8e1a
Jeff Mitchell <mitchell@kde.org>
2010-11-04 15:39:35 -0400

    Pedantic whitespacing for testing out hook

--- bin/update_repo_pointers.sh
+++ bin/update_repo_pointers.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd /home/git/repo-uid-mappings
+
 # These copy the names in the each-repo-has-it kde-repo-uid file and 
 # each-repo-may-have-it kde-repo-nick file into files in /home/git/metadata-tree
 # so that the commits URL resolver can use it to look up the actual URL
