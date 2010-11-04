2cb473ee0518df6f078e95a6345f635aa9d4bb5c
Jeff Mitchell <mitchell@kde.org>
2010-11-04 15:40:33 -0400

    Commenting

--- bin/immediate-gitweb-update.sh
+++ bin/immediate-gitweb-update.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# Makes gitweb update immediately so there are no 404s because it hasn't updated yet.
 ssh projects.kde.org "cd /repositories/$1; git remote update; git update-server-info" > /dev/null
