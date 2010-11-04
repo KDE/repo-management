fe306f3098b7c72c7b82fe14b93a618d6ce2ca7f
Jeff Mitchell <mitchell@kde.org>
2010-11-04 15:37:18 -0400

    Fix typo

--- bin/immediate-gitweb-update.sh
+++ bin/immediate-gitweb-update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh projects.kde.org "cd /repositories/$1.git; git remote update; git update-server-info"
+ssh projects.kde.org "cd /repositories/$1; git remote update; git update-server-info"
