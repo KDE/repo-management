4bd2e9d4aab8c5e6505c8cd3b3355de4c5d1646a
Jeff Mitchell <mitchell@kde.org>
2010-11-05 12:06:42 -0400

    Update file names to genericize for anongit

--- bin/push_projects_metadata.sh
+++ bin/push_projects_metadata.sh
@@ -2,19 +2,19 @@
 
 # This creates the list of files to rsync down to the anon mirror. These files are metadata living in the various git repository directories.
 cd ~git
-rm projects-repos-build.tmp
+rm anongit-repos-build.tmp
 for dir in `cat projects-to-anongit.list`
   do
-    echo "$dir/cloneurl" >> projects-repos-build.tmp
-    echo "$dir/description" >> projects-repos-build.tmp
-    echo "$dir/git-daemon-export-ok" >> projects-repos-build.tmp
-    echo "$dir/gl-creater" >> projects-repos-build.tmp
-    echo "$dir/gl-owner" >> projects-repos-build.tmp
-    echo "$dir/gl-perms" >> projects-repos-build.tmp
-    echo "$dir/kde-cloned-from" >> projects-repos-build.tmp
-    echo "$dir/kde-repo-nick" >> projects-repos-build.tmp
-    echo "$dir/kde-repo-uid" >> projects-repos-build.tmp
+    echo "$dir/cloneurl" >> anongit-repos-build.tmp
+    echo "$dir/description" >> anongit-repos-build.tmp
+    echo "$dir/git-daemon-export-ok" >> anongit-repos-build.tmp
+    echo "$dir/gl-creater" >> anongit-repos-build.tmp
+    echo "$dir/gl-owner" >> anongit-repos-build.tmp
+    echo "$dir/gl-perms" >> anongit-repos-build.tmp
+    echo "$dir/kde-cloned-from" >> anongit-repos-build.tmp
+    echo "$dir/kde-repo-nick" >> anongit-repos-build.tmp
+    echo "$dir/kde-repo-uid" >> anongit-repos-build.tmp
 done
-export RSYNC_CONNECT_PROG='ssh -l git projects.kde.org nc6 localhost 873'
+export RSYNC_CONNECT_PROG='ssh -l git anongit.kde.org nc6 localhost 873'
 # This should go to ~git/metadata-tree
-rsync -avz --password-file=/home/git/rsync_push_to_projects.secret --files-from="/home/git/projects-repos-build.tmp" /srv/kdegit/repositories git-projects@localhost::metadata-tree
+rsync -avz --password-file=/home/git/rsync_push_to_anongit.secret --files-from="/home/git/anongit-repos-build.tmp" /srv/kdegit/repositories git-anongit@localhost::metadata-tree
