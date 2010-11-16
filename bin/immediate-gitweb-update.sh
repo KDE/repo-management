b06ca6ade34ba9421f1b35ad8aadcf09226c808e
Jeff Mitchell <mitchell@kde.org>
2010-11-16 12:55:59 -0500

    Explicitly exit with 0

--- bin/immediate-gitweb-update.sh
+++ bin/immediate-gitweb-update.sh
@@ -4,3 +4,4 @@
 ssh anongit1.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
 ssh anongit2.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
 ssh anongit3.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
+exit 0
