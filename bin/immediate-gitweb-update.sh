bcb85aacf8ecf163b34563b48b0e757282baa2cf
Jeff Mitchell <mitchell@kde.org>
2010-11-16 12:42:28 -0500

    Documentation update

--- bin/immediate-gitweb-update.sh
+++ bin/immediate-gitweb-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Makes gitweb update immediately so there are no 404s because it hasn't updated yet.
+# Makes gitweb update immediately so there are no 404s on the commit URLs because of the repo not being updated yet.
 ssh anongit1.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
 ssh anongit2.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
 ssh anongit3.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
