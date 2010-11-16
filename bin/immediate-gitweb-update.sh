01ee19aa744595abf1778c0521aa466cbc008440
Jeff Mitchell <mitchell@kde.org>
2010-11-16 12:36:29 -0500

    Make the gitweb poking async

--- bin/immediate-gitweb-update.sh
+++ bin/immediate-gitweb-update.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # Makes gitweb update immediately so there are no 404s because it hasn't updated yet.
-ssh projects.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
+ssh anongit1.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
+ssh anongit2.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
+ssh anongit3.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
