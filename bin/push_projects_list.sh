7099a263db14eee817d3c59ad05bdb10c17087fc
Jeff Mitchell <mitchell@kde.org>
2010-11-17 15:49:17 -0500

    Revert to nc6

--- bin/push_projects_list.sh
+++ bin/push_projects_list.sh
@@ -2,7 +2,7 @@
 
 # This script is used by git.kde.org to push the current projects list down to the anonymous mirror; new
 # mirrors can be added by copying the block below with different hostnames
-export RSYNC_CONNECT_PROG='ssh -l git %H netcat localhost 873'
+export RSYNC_CONNECT_PROG='ssh -l git %H nc6 localhost 873'
 cd ~git
 cp projects.list projects-to-anongit.list
 rsync -avz --password-file=/home/git/rsync_push_to_anongit.secret projects-to-anongit.list git-anongit@anongit1.kde.org::projects-list
