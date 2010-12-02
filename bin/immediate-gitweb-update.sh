08e647346bbf932fa0590861e9cc2ee43ca1024f
Jeff Mitchell <mitchell@kde.org>
2010-12-02 17:06:22 -0500

    Remove dying toast from immediate update; only the machine actually hosting gitweb for now. Why isn't it acting async :-(

--- bin/immediate-gitweb-update.sh
+++ bin/immediate-gitweb-update.sh
@@ -2,6 +2,6 @@
 
 # Makes gitweb update immediately so there are no 404s on the commit URLs because of the repo not being updated yet.
 ssh anongit1.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
-ssh anongit2.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
-ssh anongit3.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
+#ssh anongit2.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
+#ssh anongit3.kde.org "cd /repositories/$1; git remote update; git update-server-info" 1>/dev/null 2>/dev/null
 exit 0
