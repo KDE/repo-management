98f78e8885551b578203d6eda208da14c0e4181a
Jeff Mitchell <mitchell@kde.org>
2010-12-06 18:15:15 -0500

    Use nc instead of nc6

--- bin/push_projects_metadata.sh
+++ bin/push_projects_metadata.sh
@@ -15,7 +15,7 @@ for dir in `cat projects-to-anongit.list`
     echo "$dir/kde-repo-nick" >> anongit-repos-build.tmp
     echo "$dir/kde-repo-uid" >> anongit-repos-build.tmp
 done
-export RSYNC_CONNECT_PROG='ssh -l git %H nc6 localhost 873'
+export RSYNC_CONNECT_PROG='ssh -l git %H nc localhost 873'
 # This should go to ~git/metadata-tree
 rsync -avz --password-file=/home/git/rsync_push_to_anongit.secret --files-from="/home/git/anongit-repos-build.tmp" /srv/kdegit/repositories git-anongit@anongit1.kde.org::metadata-tree
 rsync -avz --password-file=/home/git/rsync_push_to_anongit.secret --files-from="/home/git/anongit-repos-build.tmp" /srv/kdegit/repositories git-anongit@anongit2.kde.org::metadata-tree
