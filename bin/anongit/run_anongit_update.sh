b2f2522094580df1fc84ad61f1d4b316034c43ba
Jeff Mitchell <mitchell@kde.org>
2010-12-16 00:15:19 -0500

    Don't put anything in /etc, not necessary any more

--- bin/anongit/run_anongit_update.sh
+++ bin/anongit/run_anongit_update.sh
@@ -2,8 +2,8 @@
 
 grep `cat /home/git/thisanongithost` /home/git/repo-management/config/enabled_anongits.cfg >/dev/null || exit 1
 
-rsync -avz git.kde.org::projects-list/projects-to-anongit.list /etc/kdegit/projects-to-anongit.list
-chmod 755 /etc/kdegit/projects-to-anongit.list
+rsync -avz git.kde.org::projects-list/projects-to-anongit.list /home/git/projects-to-anongit.list.new
+chmod 755 /home/git/projects-to-anongit.list.new
 
 rm -rf /home/git/metadata-tree/*
 rsync -avz git.kde.org::metadata-tree/ /home/git/metadata-tree/
--- bin/anongit/update_repo_mirrors.sh
+++ bin/anongit/update_repo_mirrors.sh
@@ -1,9 +1,8 @@
 #/bin/sh
 
 # First, see if there are any repos that are gone and should be removed. This gets a list of just
-# the items that have been removed from the projects-to-anongit.list file which was put into /etc/kdegit
-# by rsync from git.kde.org
-diff ~/projects-to-anongit.list /etc/kdegit/projects-to-anongit.list | grep "<" | cut -c 3- > ~/diffout
+# the items that have been removed from the projects-to-anongit.list file which was put into projects-to-anongit.list.new
+diff ~/projects-to-anongit.list /home/git/projects-to-anongit.list.new | grep "<" | cut -c 3- > ~/diffout
 
 # Is it not empty?
 if [ -s ~/diffout ]
@@ -18,7 +17,7 @@ if [ -s ~/diffout ]
 fi
 
 # Now, update this to our local copy in ~git
-cp /etc/kdegit/projects-to-anongit.list ~/projects-to-anongit.list
+cp /home/git/projects-to-anongit.list.new ~/projects-to-anongit.list
 
 # Go through each line and if the repo exists and isn't an empty folder,
 # do an update. Otherwise do a clone.
