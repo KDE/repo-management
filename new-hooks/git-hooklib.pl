8f37c63469e41d77e439d94641e8ff7b8d668c33
Ben Cooksley <bcooksley@kde.org>
2010-12-20 12:01:29 +1300

    The beginning of our new hooks

---
+++ new-hooks/git-hooklib.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/perl -w
+
+sub get_revision_list
+{
+    # Initialisation....
+    my $oldsha1 = shift;
+    my $newsha1 = shift;
+    my @revision_list = ( );
+    my $revision_span = "";
+
+    # Build the revision span git will use to help build the revision list...
+    if( $change_type eq "create" ) {
+        $revision_span = "$newsha1 " . `git for-each-ref --format='^%(objectname)' refs/heads/`;
+    } else {
+        $revision_span = "$oldsha1..$newsha1";
+    }
+
+    # Read in the revision list...
+    open(GITIN, "-|") || exec( "git rev-list --reverse --topo-order $revision_span" );
+    while(<GITIN>) {
+        push( @revision_list, $_ );
+    }
+    close(GITIN);
+
+    return @revision_list;
+}
+
+sub get_commit_diffstat
+{
+    # Retrieve information....
+    my $commit = shift;
+    my $diffstat = ();
+
+    # Read some commit metadata...
+    open(GITIN, "-|") || exec( 'git', 'log', '--encoding=UTF-8', '--pretty=format:%an%n%ae%n%aD%n%B', '-n1',  $commit );
+    while(<GITIN>) {
+        next if $_ eq "";
+        push(@diffstat, $_);
+    }
+    close(GITIN);
+
+    return $diffstat
+}
+
+sub get_commit_details
+{
+    # Retrieve information....
+    my $commit = shift;
+    my ($author_name, $author_email, $date, $message);
+
+    # Read some commit metadata...
+    open(GITIN, "-|") || exec( 'git', 'log', '--encoding=UTF-8', '--pretty=format:%an%n%ae%n%aD%n%B', '-n1',  $commit );
+    $author_name = <GITIN>;
+    $author_email = <GITIN>;
+    $date = <GITIN>;
+    while(<GITIN>) {
+        push(@message, $_);
+    }
+    close(GITIN);
+
+    return { 'author_name' => $author_name, 'author_email' => $author_email, 'date' => $date, 'message' => $message };
+}
+
+sub get_commit_changed_files
+{
+    # Initialise and retrieve info...
+    my $commit = shift;
+    my $files = ();
+
+    # A question of do or don't here.... read into memory or piece by piece??
+    open(GITIN, "-|") || exec( 'git', 'show', '--pretty="format:"', '--name-only', $commit );
+    while(<GITIN>) {
+        next if $_ eq "";
+        push(@files, $_);
+    }
+    close(GITIN);
+
+    return $files;
+}
+
+sub initialise_audit
+{
+}
+
+sub audit_eol
+{
+    # Check to see if we need to run on this repository....
+    if( -e "$auditcfg/skip-eol" ) {
+        return;
+    }
+}
+
+sub audit_filename
+{
+    # Check to see if we need to run on this repository....
+    if( -e "$auditcfg/skip-author" ) {
+        return;
+    }
+}
+
+sub audit_author
+{
+    # Check to see if we need to run on this repository....
+    if( -e "$auditcfg/skip-filename" ) {
+        return;
+    }
+}
+
+return 1;
