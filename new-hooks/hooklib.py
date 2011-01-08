01d3087f17a1f13dd6319756ea567645d3d6074a
Ben Cooksley <bcooksley@kde.org>
2011-01-09 00:05:48 +1300

    Port auditing to new hooks. Extract the list of changed files instead of a diff stat in the data extraction phase, which is faster too. Misc fixes + todo update.

--- new-hooks/hooklib.py
+++ new-hooks/hooklib.py
@@ -3,6 +3,7 @@
 import os
 import re
 import subprocess
+import dns.resolver
 
 class RepoType:
     "Enum type - Indicates the type of repository"
@@ -38,7 +39,13 @@ class Repository:
         self.old_sha1 = old_sha1
         self.new_sha1 = new_sha1
         self.push_user = push_user
-        self.commits = []
+        self.commits = dict()
+        
+        # Find our configuration directory....
+        if os.getenv('REPO_MGMT'):
+            self.management_directory = os.getenv('REPO_MGMT')
+        else:
+            self.management_directory = os.getenv('HOME') + "/repo-management"
         
         # Set path and id....
         path_match = re.match("^/home/ben/sysadmin/(.+).git$", os.getenv('GIT_DIR'))
@@ -72,13 +79,13 @@ class Repository:
              ('D'  , ('%aD%n', '(?P<date>.+)\n')),
              ('CN' , ('%cn%n', '(?P<committer_name>.+)\n')),
              ('CE' , ('%ce%n', '(?P<committer_email>.+)\n')),
-             ('MSG', ('%B%xff','(?P<message>(.|\n)+)\xff(?P<diff_stat>(.|\n)*)'))
+             ('MSG', ('%B%xff','(?P<message>(.|\n)+)\xff(?P<files_changed>(.|\n)*)'))
             )
         pretty_format = ''.join([': '.join((i,j[0])) for i,j in l])
         re_format = '^' + ''.join([': '.join((i,j[1])) for i,j in l]) + '$'
 
         # Get the data we are going to be parsing....
-        log_arguments = "--stat --reverse -z --pretty=format:'{0}'".format(pretty_format)
+        log_arguments = "--name-only --reverse -z --pretty=format:'{0}'".format(pretty_format)
         command = "git log {0}..{1} {2}".format(self.old_sha1, self.new_sha1, log_arguments) # For debugging...
         #command = "git rev-parse --not --all | grep -v {0} | git log --stdin --no-walk {1} {2}".format(self.old_sha1, log_arguments, revision_span)
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -89,11 +96,16 @@ class Repository:
             return
         
         # Parse time!
-        for commit_data in output.split("\x00"):
+        for commit_data in output.split("\x00\x00"):
             match = re.match(re_format, commit_data, re.MULTILINE)
             commit = Commit()
             commit.__dict__.update(match.groupdict())
-            self.commits.append( commit )
+            self.commits[ commit.sha1 ] = commit
+            
+        # Cleanup files_changed....
+        for (sha1, commit) in self.commits.iteritems():
+            clean_list = commit.files_changed.split("\x00")
+            commit.files_changed = [filename.strip() for filename in clean_list]
             
     def __write_metadata(self):
         metadata = file(os.getenv('GIT_DIR') + "/cloneurl", "w")
@@ -162,6 +174,104 @@ class Repository:
             return ChangeType.Forced
         else:
             return ChangeType.Update
+            
+class CommitAuditor:
+    "Performs all audits on commits"
+    def __init__(self, repository):
+        self.repository = repository
+        self.failures = dict()
+        self.__setup_filenames()
+            
+    def __log_failure(self, commit, message):
+        if not self.failures.has_key( commit ):
+            self.failures[ commit ] = []
+            
+        self.failures[ commit ].append( message )
+        
+    def __setup_filenames(self):
+        self.filename_limits = []
+        configuration = open(self.repository.management_directory + "/config/blockedfiles.cfg")
+        for line in configuration:
+            regex = line.strip()
+
+            # Skip comments and blank lines
+            if regex.startswith("#") or len(regex) == 0:
+                continue
+            
+            restriction = re.compile(regex)
+            if restriction:
+                self.filename_limits.append( restriction )
+
+        configuration.close()
+        
+    def audit_eol(self):
+        # Regex's....
+        re_commit = re.compile("^\x00(.+)\x00$")
+        re_filename = re.compile("^diff --git a\/(\S+) b\/(\S+)$")
+        blocked_eol = re.compile(r"(?:\r\n|\n\r|\r)$")
+        
+        # Get the full diff for all commits...
+        commit_list = ' '.join( self.repository.commits.keys() )
+        command = "git log -p --reverse --pretty=format:%x00%H%x00 --stdin"
+        process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        for sha1 in self.repository.commits.keys():
+            process.stdin.write(sha1 + "\n")
+        process.stdin.close()
+        
+        # Do EOL audit!
+        for line in process.stdout:
+            commit_change = re.match( re_commit, line )
+            if commit_change:
+                commit = commit_change.group(1)
+                continue
+
+            file_change = re.match( re_filename, line )
+            if file_change:
+                filename = file_change.group(2)
+                violation = False
+                continue
+
+            # Don't complain about the same file twice...
+            if violation:
+                continue
+
+            # Unless they added it, ignore it
+            if not line.startswith("+"):
+                continue
+
+            if re.search( blocked_eol, line ):
+                # Failure has been found... handle it
+                violation = True
+                self.__log_failure(commit, "End Of Line Style - " + filename);
+                
+    def audit_filename(self):
+        for commit in self.repository.commits.values():
+            for filename in commit.files_changed:
+                for restriction in self.filename_limits:
+                    if re.search(restriction, filename):
+                        self.__log_failure(commit.sha1, "File Name - " + filename)
+                        
+    def audit_metadata(self):
+        # Iterate over commits....
+        disallowed_domains = ["localhost", "localhost.localdomain", "(none)"]
+        for commit in self.repository.commits.values():
+            for email_address in [ commit.committer_email, commit.author_email ]:
+                # Extract the email address, and reject them if extraction fails....
+                extraction = re.match("^(\S+)@(\S+)$", email_address)
+                if not extraction:
+                    self.__log_failure(commit.sha1, "Email Address - " + email_address)
+                
+                # Don't allow domains which are disallowed...
+                domain = extraction.group(2)
+                if domain in disallowed_domains:
+                    self.__log_failure(commit.sha1, "Email Address - " + email_address)
+
+                # Ensure they have a valid MX/A entry in DNS....
+                try:
+                    mx_results = dns.resolver.query(domain, 'MX')
+                    a_results  = dns.resolver.query(domain, 'A')
+                except dns.resolver.NXDOMAIN:
+                    self.__log_failure(commit.sha1, "Email Address - " + email_address)
 
 class Commit:
     "Represents a git commit"
