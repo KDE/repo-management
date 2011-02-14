e377098b1e076d4e3648544fcc955aa1df2391f1
Ben Cooksley <bcooksley@kde.org>
2011-02-15 12:59:55 +1300

    Git Reviewboard now enforces use of SSL

--- hooks/update_review.py
+++ hooks/update_review.py
@@ -28,7 +28,7 @@ from rest_client import Connection
 
 # Basic constants
 
-REVIEWBOARD_URL = "http://git.reviewboard.kde.org"
+REVIEWBOARD_URL = "https://git.reviewboard.kde.org"
 DEFAULT_LEVEL = logging.INFO
 
 
