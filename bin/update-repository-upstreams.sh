2da67f2e4fe95c67590f90c99f3697e8bbeff77d
Ben Cooksley <bcooksley@kde.org>
2011-06-09 19:47:40 +1200

    Minor fix

--- bin/update-repository-upstreams.sh
+++ bin/update-repository-upstreams.sh
@@ -2,7 +2,7 @@ mgmtdir="/home/git/repo-management"
 repobase="/srv/kdegit/repositories"
 
 for upstreamrepo in `ls $mgmtdir/repo-configs/upstream/`; do
-    remoteurl=`cat $upstreamrepo`
+    remoteurl=`cat $mgmtdir/repo-configs/upstream/$upstreamrepo`
     repo=$repobase/$upstreamrepo
     cd $repo
     git fetch -n "$remoteurl" +refs/heads/*:refs/upstream/heads/* +refs/tags/*:refs/upstream/tags/*
