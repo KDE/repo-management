bbee4af1902fbcf122e44f698d75f3f49e82480f
Ben Cooksley <bcooksley@kde.org>
2011-09-20 16:55:19 +1200

    Refactor out tarball creation function, and change the initrepo.sh script to use /bin/sh instead of /bin/bash BUG: 282320

--- bin/anongit/create_tarballs.rb
+++ bin/anongit/create_tarballs.rb
@@ -1,63 +1,91 @@
 #!/usr/bin/env ruby
-
 require 'digest/sha1'
 
-# A so far quick and dirty hack to do the tarball generation
-# It uses Ruby to make some things easier and shells out for most of it
-# Found the printAndDescent code on rosettacode and adopted it
-
-#define a recursive function that will traverse the directory tree
+# Define a recursive function that will traverse the directory tree
 def printAndDescend(pattern, directory=nil)
-  #we keep track of the directories, to be used in the second, recursive part of this function
   if not directory
     directory = '*'
   end
+  
+  # We keep track of the directories, to be used in the second, recursive part of this function
   directories=[]
+  gitDirectoryRoot = ARGV[0]
+  tarballDirectoryRoot = ARGV[1]
+
   Dir[directory].sort.each do |name|
-    if File.directory?(name) and name[pattern] and not File.expand_path(name)["#{ARGV[0]}/scratch"] and not File.expand_path(name)["#{ARGV[0]}/clones"] and not File.expand_path(name)["#{ARGV[0]}/sysadmin"]
-      # do magic here
-      # first, garbage collect the repo so that we have the smallest tarball
-      path = File.expand_path(name)
-      newpath = path.gsub(/#{ARGV[0]}/, '')
-      newpath.gsub!(/\.git/, '')
-      if newpath.nil?
-        puts "BAILING!"
-        Process.exit!(1) 
-      end
-      puts "Working on repository in #{path}"
-      basename = File.basename(newpath)
-      puts "Will build tarball into #{ARGV[1]}#{newpath}/#{basename}-latest.tar.gz"
-      # TODO: I'm not actually sure the gc is needed; do fresh checkouts take after the server, or will they be
-      # compact automatically since they're a fresh write of the refs to the local repo?
-      # Need to investigate but since everything right now is already
-      # garbage collected I can't at the moment...  :-)
-      next if not File.exists?("#{File.expand_path(name)}/git-daemon-export-ok")
-      Dir.chdir(File.expand_path(name)){ %x[git gc --auto] }
-      Dir.chdir(File.expand_path(name)){ %x[git prune] }
-      Dir.chdir("#{ARGV[1]}/"){ %x[mkdir -p  #{ARGV[1]}#{newpath}] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[git clone -l #{ARGV[0]}#{newpath}.git #{basename}] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[rm -rf .git] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[mv #{basename}/.git .] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[rm -rf #{basename}] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[mkdir #{basename}] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[mv .git #{basename}] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}/#{basename}") { %x[sed -i -e "s/\\/srv\\/kdegit\\/repositories/http\\:\\/\\/anongit.kde.org/g" .git/config] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}/#{basename}") { %x[echo "#!/bin/bash\n\nrm initrepo.sh\n\ngit reset --hard HEAD" > initrepo.sh; chmod +x initrepo.sh] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[rm -rf *.tar.gz] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[tar -czf #{basename}-latest-stage.tar.gz #{basename}; rm -rf #{basename}] }
-      time = Time.now
-      timestring = time.strftime("%Y%m%d%H%M%S")
-      digest = Digest::SHA1.file("#{ARGV[1]}/#{newpath}/#{basename}-latest-stage.tar.gz").hexdigest
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[ln -s #{basename}_#{timestring}_sha1-#{digest}.tar.gz #{basename}-latest.tar.gz] }
-      Dir.chdir("#{ARGV[1]}/#{newpath}") { %x[mv #{basename}-latest-stage.tar.gz #{basename}_#{timestring}_sha1-#{digest}.tar.gz] }
+    fullPath = File.expand_path(name)
+    if File.directory?(name) and name[pattern] and not fullPath["#{gitDirectoryRoot}/scratch"] and not fullPath["#{gitDirectoryRoot}/clones"] and not fullPath["#{gitDirectoryRoot}/sysadmin"]
+      buildTarball(name, gitDirectoryRoot, tarballDirectoryRoot)
     elsif File.directory?(name)
       directories << name
     end
   end
+
   directories.each do |name|
     #don't descend into . or .. on linux
     Dir.chdir(name){printAndDescend(pattern)} if !Dir.pwd[File.expand_path(name)]
   end
+end
+
+# Function to build a tarball of a git repository
+def buildTarball(repository, gitDirectoryRoot, tarballDirectoryRoot)
+    # Make sure we are allowed to build a tarball for this repo
+    fullRepoPath = File.expand_path(repository)
+    return if not File.exists?("#{fullRepoPath}/git-daemon-export-ok")
+    
+    # Get a cleaned name suitable for use
+    repoPath = fullRepoPath.gsub(/#{gitDirectoryRoot}/, '')
+    repoPath.gsub!(/\.git/, '')
+    repoName = File.basename(repoPath)
+    if repoPath.nil?
+        puts "Encountered error while processing repository #{repository}\n"
+        return
+    end
+    
+    # Start the processing for this repository
+    tarballPath = "#{tarballDirectoryRoot}/#{repoPath}"
+    puts "Ready to build tarball for repository #{repository}"
+    puts "Tarball to be built into #{tarballPath}/#{repoName}-latest.tar.gz"
+
+    # TODO: I'm not actually sure the gc is needed; do fresh checkouts take after the server, or will they be
+    # compact automatically since they're a fresh write of the refs to the local repo?
+    Dir.chdir(fullRepoPath) {
+        %x[git gc --auto]
+        %x[git prune]
+    }
+    
+    # Ensure the working area exists...
+    %x[mkdir -p  #{tarballPath}]
+    
+    # Clone the repository, then remove all the files in the checkout except for the .git/ directory
+    Dir.chdir(tarballPath) {
+        %x[git clone -l #{fullRepoPath} #{repoName}] 
+        %x[rm -rf .git]
+        %x[mv #{repoName}/.git .]
+        %x[rm -rf #{repoName}]
+        %x[mkdir #{repoName}]
+        %x[mv .git #{repoName}] 
+    }
+    
+    # Alter the git repository configuration to use anongit.kde.org, and add the initrepo.sh script
+    Dir.chdir("#{tarballPath}/#{repoName}") { 
+        %x[sed -i -e "s/\\/srv\\/kdegit\\/repositories/http\\:\\/\\/anongit.kde.org/g" .git/config]
+        %x[echo "#!/bin/sh\n\nrm initrepo.sh\n\ngit reset --hard HEAD" > initrepo.sh; chmod +x initrepo.sh]
+    }
+
+    # Remove the old tarball, and generate the new one
+    Dir.chdir(tarballPath) { 
+        %x[rm -rf *.tar.gz] 
+        %x[tar -czf #{repoName}-latest-stage.tar.gz #{repoName}; rm -rf #{repoName}]
+    }
+
+    # Get a timestamp, and hash the file, then setup the final tarball name + symlink
+    timestring = Time.now.strftime("%Y%m%d%H%M%S")
+    digest = Digest::SHA1.file("#{tarballPath}/#{repoName}-latest-stage.tar.gz").hexdigest  
+    Dir.chdir(tarballPath) {
+        %x[ln -s #{repoName}_#{timestring}_sha1-#{digest}.tar.gz #{repoName}-latest.tar.gz]
+        %x[mv #{repoName}-latest-stage.tar.gz #{repoName}_#{timestring}_sha1-#{digest}.tar.gz] 
+    }
 end
 
 if not ARGV[0] or not ARGV[1]
