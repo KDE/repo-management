629e580adc98bf07d833bfd119536bfdd17acf24
Jeff Mitchell <mitchell@kde.org>
2012-01-05 17:38:01 -0500

    Account for prefix

--- bin/anongit/create_anongit_metadata.sh
+++ bin/anongit/create_anongit_metadata.sh
@@ -7,16 +7,16 @@ chmod 755 projects-list/projects-to-anongit.list
 rm anongit-repos-build.tmp
 for dir in $(cat projects-list/projects-to-anongit.list)
   do
-    [ -e $dir/cloneurl ] && echo "$dir/cloneurl" >> anongit-repos-build.tmp
-    [ -e $dir/description ] && echo "$dir/description" >> anongit-repos-build.tmp
-    [ -e $dir/git-daemon-export-ok ] && echo "$dir/git-daemon-export-ok" >> anongit-repos-build.tmp
-    [ -e $dir/gl-creater ] && echo "$dir/gl-creater" >> anongit-repos-build.tmp
-    [ -e $dir/gl-conf ] && echo "$dir/gl-conf" >> anongit-repos-build.tmp
-    [ -e $dir/gl-owner ] && echo "$dir/gl-owner" >> anongit-repos-build.tmp
-    [ -e $dir/gl-perms ] && echo "$dir/gl-perms" >> anongit-repos-build.tmp
-    [ -e $dir/kde-cloned-from ] && echo "$dir/kde-cloned-from" >> anongit-repos-build.tmp
-    [ -e $dir/kde-repo-nick ] && echo "$dir/kde-repo-nick" >> anongit-repos-build.tmp
-    [ -e $dir/kde-repo-uid ] && echo "$dir/kde-repo-uid" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/cloneurl ] && echo "$dir/cloneurl" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/description ] && echo "$dir/description" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/git-daemon-export-ok ] && echo "$dir/git-daemon-export-ok" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/gl-creater ] && echo "$dir/gl-creater" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/gl-conf ] && echo "$dir/gl-conf" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/gl-owner ] && echo "$dir/gl-owner" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/gl-perms ] && echo "$dir/gl-perms" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/kde-cloned-from ] && echo "$dir/kde-cloned-from" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/kde-repo-nick ] && echo "$dir/kde-repo-nick" >> anongit-repos-build.tmp
+    [ -e /srv/kdegit/repositories/$dir/kde-repo-uid ] && echo "$dir/kde-repo-uid" >> anongit-repos-build.tmp
 done
 rsync -avz --delete --files-from="/home/git/anongit-repos-build.tmp" /srv/kdegit/repositories /home/git/metadata-tree
 
