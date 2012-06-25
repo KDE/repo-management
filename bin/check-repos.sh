5076b00603933b5670a3022362c0b9b28c17b4c0
Ben Cooksley <bcooksley@kde.org>
2012-06-25 19:04:47 +1200

    Add script which runs on git.kde.org to inform sysadmin of repositories whose hooks are still disabled

---
+++ bin/check-repos.sh
@@ -0,0 +1,36 @@
+cd /srv/kdegit/repositories/
+DATA=''
+for repo in $(find -maxdepth 2 -name "*.git" -type d); do
+    export GIT_DIR=$repo/
+
+    list=$(git branch)
+    list2=$(git tag)
+    if [ "$list" == "" -a "$list2" == "" ]; then
+        repofilled=0
+    else
+        repofilled=1
+    fi
+
+    if [ ! -f $repo/gl-creater -a -f $repo/kde-hooks-off ]; then
+        # Hooks are off in these tests....
+        if [ $repofilled == 0 ]; then
+            # This case is for repos prior to being filled and is normal
+            DATA="$repo is unused and has hooks disabled.\n$DATA"
+        else
+            # Forgot to switch off after filling...
+            DATA="$repo is USED with hooks DISABLED (POLICY VIOLATION)\n$DATA"
+        fi
+    else
+        # Hooks are on in these tests....
+        if [ ! -f $repo/gl-creater -a $repofilled == 0 ]; then
+            # May cause a flood!!!
+            DATA="$repo is NOT USED with hooks ENABLED (FLODD WARNING)\n$DATA"
+        fi
+        # The other case here is normal, so we don't look for that
+        # We ignore repos with a gl-creater file, as they are clones/scratch
+    fi
+done
+
+if [ "$DATA" != "" ]; then
+    echo -e $DATA | mail -r "sysadmin@kde.org" -s "Git repository hooks-disabled check" sysadmin@kde.org
+fi
