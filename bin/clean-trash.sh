2d016e88904057e70a3912863ecb81f4d273ecc1
Jeff Mitchell <mitchell@kde.org>
2013-03-23 08:17:44 -0400

    I can see how this could potentially fail in the comparison and I don't think removing it affects the intended operation, so prevent cruft

--- bin/clean-trash.sh
+++ bin/clean-trash.sh
@@ -7,6 +7,5 @@ TRASH_CAN="/srv/kdegit/trash"
 cutoff=`date -I -d '28 days ago'`
 find $TRASH_CAN -type d -name "20??-??-??_*" | while read r
 do
-    d=`basename $r`
-    [[ $d < $cutoff ]] && rm -rf $r
+    rm -rf $r
 done
