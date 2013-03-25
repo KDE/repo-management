66a6efc45dd2e6b2e4f76d71e4e64a09583596ff
Jeff Mitchell <mitchell@kde.org>
2013-03-25 10:42:46 -0400

    Add more sanity checking

---
+++ bin/generate_project_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+/home/git/bin/gitolite trigger POST_COMPILE
+/home/git/repo-management/bin/verify_new_projects_list.pl /home/git/projects.list.candidate /home/git/projects.list
+
+if [ $? -ne 0 ]
+then
+  echo "Projects list file may have changed too much; not replacing current list" | mail -r "sysadmin@kde.org" -s "WARNING: projects.list problem" sysadmin@kde.org
+  exit 1
+fi
+
+mv /home/git/projects.list.candidate /home/git/projects.list
--- bin/verify_new_projects_list.pl
+++ bin/verify_new_projects_list.pl
@@ -1,12 +1,29 @@
 #!/usr/bin/env perl
 
-my $prevlines=`wc $ENV{HOME}/projects.list`;
-my $candidatelines=`wc $ENV{HOME}/projects.list.candidate`;
+my $prevlines = $ARGV[0];
+my $candidatelines = $ARGV[1];
 
-my $diff = abs(($candidatelines - $prevlines) / (1.0 * $prevlines)) * 100;
+my $wcprevlines = `wc -l $prevlines`;
+my $wccandidatelines = `wc -l $candidatelines`;
+my $difflines = `diff --suppress-common-lines $prevlines $candidatelines | wc -l`;
 
-if ($diff > 1.0) {
-	exit(1);
+my $wcdiff = abs(($wccandidatelines - $wcprevlines) / (1.0 * $wcprevlines)) * 100;
+
+my $diffprev = abs($difflines * 1.0 / $wcprevlines);
+my $diffcandidate = abs($difflines * 1.0 / $wccandidatelines);
+
+if ($wcdiff > 1.0) {
+  exit(1);
+}
+
+# Higher tolerance on these checks since diff outputs diff output
+
+if ($diffprev > 0.02) {
+  exit(2);
+}
+
+if ($diffcandidate > 0.02) {
+  exit(3);
 }
 
 exit(0);
