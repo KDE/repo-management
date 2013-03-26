f594743eeb15fa1e4a86d569656a8d53f84d4afe
Jeff Mitchell <mitchell@kde.org>
2013-03-26 17:16:21 -0400

    Oops, fix argument selector

--- bin/clean-trash.sh
+++ bin/clean-trash.sh
@@ -4,7 +4,7 @@
 # As you might expect.
 
 cutoff=`date -I -d '28 days ago'`
-find $0 -type d -name "20??-??-??_*" | while read r
+find $1 -type d -name "20??-??-??_*" | while read r
 do
     rm -rf $r
 done
