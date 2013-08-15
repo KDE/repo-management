9ea67efb3beb69e6fc6ad54eb234ec2e042c6fe6
Ben Cooksley <bcooksley@kde.org>
2013-08-15 20:04:32 +1200

    Fix in both places

--- bin/repository-mirror-updater.py
+++ bin/repository-mirror-updater.py
@@ -12,7 +12,7 @@ except IndexError:
 
 # Perform the general repository update
 command = "git fetch -n '{0}' +refs/heads/*:refs/heads/* +refs/tags/*:refs/tags/*".format( remoteUrl )
-subprocess.call(command, shell=True, stdout=subprocess.STDOUT, stderr=subprocess.STDERR)
+subprocess.call(command, shell=True, stdout=subprocess.STDOUT, stderr=subprocess.STDOUT)
 
 # Now we perform the HEAD update, first thing is to get some remote repository metadata
 command = "git remote show '{0}'".format( remoteUrl )
