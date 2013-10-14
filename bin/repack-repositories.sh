6d2cf6c45c9104b54d52caf96a6fff833422e6d9
Ben Cooksley <bcooksley@kde.org>
2013-10-14 19:35:59 +1300

    Fix missing elements

--- bin/repack-repositories.sh
+++ bin/repack-repositories.sh
@@ -6,13 +6,13 @@ cd $repopath
 for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
 
 cd /repositories/sysadmin
-for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
+for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/sysadmin/$line/; git gc; git prune; done
 
 cd /repositories/websites
-for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
+for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/websites/$line/; git gc; git prune; done
 
 cd /repositories/scratch
-for line in $( find -mindepth 2 -maxdepth 2 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
+for line in $( find -mindepth 2 -maxdepth 2 -type d -name "*.git" ); do cd $repopath/scratch/$line/; git gc; git prune; done
 
 cd /repositories/clones
-for line in $( find -mindepth 3 -maxdepth 3 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
+for line in $( find -mindepth 3 -maxdepth 3 -type d -name "*.git" ); do cd $repopath/clones/$line/; git gc; git prune; done
