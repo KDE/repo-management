e0a308190864bacb789f719a7a00e122e076122f
Ben Cooksley <bcooksley@kde.org>
2013-10-14 18:45:26 +1300

    Add script to optimise repositories on server

---
+++ bin/repack-repositories.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+repopath=$1
+
+cd $repopath
+for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
+
+cd /repositories/sysadmin
+for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
+
+cd /repositories/websites
+for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
+
+cd /repositories/scratch
+for line in $( find -mindepth 2 -maxdepth 2 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
+
+cd /repositories/clones
+for line in $( find -mindepth 3 -maxdepth 3 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
