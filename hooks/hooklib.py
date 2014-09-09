e2bdce4bab091e2059fdb528d9205b6573c5ea1f
Ben Cooksley <bcooksley@kde.org>
2014-09-10 07:28:52 +1200

    Send web emails to olios too

--- hooks/hooklib.py
+++ hooks/hooklib.py
@@ -477,6 +477,7 @@ class CommitNotifier(object):
 
         if builder.repository.repo_type == RepoType.Website:
             bcc_addresses.append( 'scmupdate@spider-mail.kde.org' )
+            bcc_addresses.append( 'scmupdate@olios.kde.org' )
 
         i18nInfraChange = re.compile("(.*)Messages.sh$")
         if any(i18nInfraChange.match(filename) for filename in builder.commit.files_changed):
