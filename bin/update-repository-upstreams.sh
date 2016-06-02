efe57e24c8392bd368d60ba83c4982f29b2cc52b
Ben Cooksley <bcooksley@kde.org>
2016-06-02 23:01:57 +1200

    No idea why we just started getting these mails

--- bin/update-repository-upstreams.sh
+++ bin/update-repository-upstreams.sh
@@ -1,13 +1,13 @@
 mgmtdir="/home/git/repo-management"
 repobase="/srv/git/repositories"
 
-for upstreamrepo in `ls $mgmtdir/repo-configs/upstream/`; do
-    remoteurl=`cat $mgmtdir/repo-configs/upstream/$upstreamrepo`
-    repo=$repobase/$upstreamrepo
-    cd $repo
-    git fetch -n "$remoteurl" +refs/heads/*:refs/upstream/heads/* +refs/tags/*:refs/upstream/tags/*
-    $mgmtdir/hooks/post-update
-done
+#for upstreamrepo in `ls $mgmtdir/repo-configs/upstream/`; do
+#    remoteurl=`cat $mgmtdir/repo-configs/upstream/$upstreamrepo`
+#    repo=$repobase/$upstreamrepo
+#    cd $repo
+#    git fetch -n "$remoteurl" +refs/heads/*:refs/upstream/heads/* +refs/tags/*:refs/upstream/tags/*
+#    $mgmtdir/hooks/post-update
+#done
 
 cd $mgmtdir/repo-configs/mirror/
 for mirrorrepo in `find -type f`; do
