10df34880def2c15cfa9d670f1baf31d64bf4a9d
Boudhayan Gupta <me@BaloneyGeek.com>
2016-07-19 14:27:02 +0530

    initial add of push-to-anongit script

---
+++ bin/sync-to-anongit.sh
@@ -0,0 +1,11 @@
+# Static Config
+mgmtdir="/home/git/repo-management"
+
+# Grab our input
+reponame="$1"
+
+# Build the remote URL up
+remoteurl="git@erised.kde.org:$reponame"
+# Now we push it up there
+cd "/srv/git/repositories/$reponame"
+git push --mirror "$remoteurl"
