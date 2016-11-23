a8a14d46129600096b833dfd2c7bdb32ac404cfe
Ben Cooksley <bcooksley@kde.org>
2016-11-23 21:01:38 +1300

    Merge server changes

--- maintenance/repack-repositories.sh
+++ maintenance/repack-repositories.sh
@@ -5,14 +5,15 @@ repopath=$1
 cd $repopath
 for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/$line/; git gc; git prune; done
 
-cd /repositories/sysadmin
+cd $repopath/sysadmin
 for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/sysadmin/$line/; git gc; git prune; done
 
-cd /repositories/websites
+cd $repopath/websites
 for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do cd $repopath/websites/$line/; git gc; git prune; done
 
-cd /repositories/scratch
+cd $repopath/scratch
 for line in $( find -mindepth 2 -maxdepth 2 -type d -name "*.git" ); do cd $repopath/scratch/$line/; git gc; git prune; done
 
-cd /repositories/clones
+cd $repopath/clones
 for line in $( find -mindepth 3 -maxdepth 3 -type d -name "*.git" ); do cd $repopath/clones/$line/; git gc; git prune; done
+
