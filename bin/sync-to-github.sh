0cedfebb0591f4c3df043e6b6afa3b5191b77a59
Boudhayan Gupta <me@BaloneyGeek.com>
2017-02-19 15:02:09 +0100

    restore some older github sync scripts

---
+++ bin/sync-to-github.sh
@@ -0,0 +1,13 @@
+# Static Config
+mgmtdir="/home/git/repo-management"
+
+# Grab our input
+reponame="$1"
+urlpath="$2"
+
+# Build the remote URL up
+remoteurl="git@github.com:kde/$reponame"
+# Make sure the repo exists on Github
+python $mgmtdir/bin/create-on-github.py "$reponame" "/srv/git/repositories/$urlpath"
+# Now we push it up there
+python $mgmtdir/helpers/update-repo-mirror.py "$reponame" "$remoteurl"
