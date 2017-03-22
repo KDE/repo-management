b75c20a16fde21eaeca903de89a78b27f4da944a
Ben Cooksley <bcooksley@kde.org>
2017-03-22 21:53:55 +1300

    Split the github stuff to it's own directory

--- github-sync/force-sync-github.sh
+++ github-sync/force-sync-github.sh
@@ -17,7 +17,7 @@ for line in $( find -mindepth 1 -maxdepth 1 -type d -name "*.git" ); do
     # Build the remote URL up
     remoteurl="git@github.com:kde/$reponame"
     # Make sure the repo exists on Github
-    python $mgmtdir/bin/create-on-github.py "$reponame" "/srv/git/repositories/$urlpath"
+    python $mgmtdir/github-sync/create-on-github.py "$reponame" "/srv/git/repositories/$urlpath"
     # Now we push it up there
     python $mgmtdir/helpers/update-repo-mirror.py "$reponame" "$remoteurl"
 
