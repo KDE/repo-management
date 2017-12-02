7664a490b62ad28ee88561b4140f78d30121b113
Ben Cooksley <bcooksley@kde.org>
2017-12-03 00:03:34 +1300

    Let's try doing it as a separate script

---
+++ helpers/trigger-jenkins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Grab our input
+urlpath="$1"
+
+# Before we can proceed, is this a blacklisted repository?
+if [[ "$urlpath" = "sysadmin/ci-tooling.git" ]] || [[ "$urlpath" = "sysadmin/repo-metadata.git" ]] || [[ "$urlpath" = "kde-build-metadata.git" ]]; then
+    exit
+fi
+
+# Now we wait 10 seconds - this allows the anongit network to sync
+sleep 10s
+
+# Tell the KDE CI system
+#curl --connect-timeout 10 --max-time 10 "https://build.kde.org/git/notifyCommit?url=git://anongit.kde.org/$urlpath" &> /dev/null
+
+# Trigger an external build system (contact santa_)
+curl --connect-timeout 10 --max-time 10 "http://gpul.grupos.udc.es:8080/git/notifyCommit?url=git://anongit.kde.org/$urlpath" &> /dev/null
+
