3c05bfc59fd037896ffe93e7c3bbbc8cb024bf5b
Ben Cooksley <bcooksley@kde.org>
2018-11-23 19:52:18 +1300

    Prepare to sync new Invent environment over to git.kde.org during the test phase

---
+++ helpers/sync-from-invent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Grab our input
+localrepository="$1"
+
+# If the repository is a kde/* repository then we need to map it to a equivalent git.kde.org path
+# This is done by removing the kde/ component
+# For websites and sysadmin repositories nothing special is needed, as the namespace for those is identical
+remoterepository=${localrepository#kde/}
+
+# Build the remote URL up
+remoteurl="git@git.kde.org:$remoterepository"
+
+# Now we push it up there
+cd "/srv/gitlab/repositories/$reponame"
+git push --mirror "$remoteurl"
