8d19ad82700306d9cca2f691f203b741d8a81271
Ben Cooksley <bcooksley@kde.org>
2018-12-13 20:50:48 +1300

    Make this generic enough to work for anongit.neon.kde.org too

--- tarball-redirector/redirect-server.rb
+++ tarball-redirector/redirect-server.rb
@@ -20,7 +20,7 @@
 require 'sinatra'
 
 get %r{/updateRepo/(.*)} do |url|
-  path = '/home/git/repositories/' + url
+  path = ENV['HOME'] + '/repositories/' + url
   if not File.exists?(path) or not File.directory?(path)
     return 'FAIL'
   end
